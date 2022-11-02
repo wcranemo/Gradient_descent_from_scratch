332037620bbf3966d05f340936d59c6b4f70e5b6
wcranemo <66427869+wcranemo@users.noreply.github.com>
2022-11-02 16:39:53 -0400

    added a regularizatoin parameter to the model to reduce overfitting

--- simple_reg.py
+++ simple_reg.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 def base_func(input):
     # return((input ** 2) + input - 3) #quadratic
-    return(5 * np.sin(input) + input - 2)
+    return(3 * input ** 3 - 10 * input ** 2 + input + 20)
 
 def create_target(x_targ):
     """x_targ is numpy array"""
@@ -63,7 +63,7 @@ def mean_unscale(scld_val, range, avg):
     """takes a mean scaled value and returns the unscaled value"""
     return(scld_val * range + avg)
 
-def gradient_descent(ws, bias, alpha, xn_normed, y_targ):
+def gradient_descent(ws, bias, alpha, reg_term, xn_normed, y_targ):
     # print(xn_normed, '\n')
     # print(xn_normed[:, 0])
     new_ws = np.empty_like(ws)
@@ -78,7 +78,8 @@ def gradient_descent(ws, bias, alpha, xn_normed, y_targ):
                 func += ws[k] * xn_normed[k][j]
             dJ_dwi += (func - y_targ[j]) * xn_normed[i][j]
         dJ_dwi /= np.shape(y_targ)[0]
-        new_ws[i] = ws[i] - (alpha * dJ_dwi)
+        regulariztion = (reg_term * ws[i]) / np.shape(y_targ)[0]
+        new_ws[i] = ws[i] - (alpha * (dJ_dwi + regulariztion))
 
     dJ_db = 0
     for y in range(np.shape(y_targ)[0]):
@@ -114,7 +115,7 @@ def create_scaled_xs(x_vals, degree, xn_rng, xn_avg):
 
     return(output)
 
-def poly_reg(x_targ, y_targ, degree):
+def poly_reg(x_targ, y_targ, degree, alpha, reg_term, num_iters):
     # x1_norm, x1_n_rng, x1_n_avg = mean_normed(x_targ, 1)
     # x2_norm, x2_n_rng, x2_n_avg = mean_normed(x_targ, 2)
 
@@ -130,44 +131,40 @@ def poly_reg(x_targ, y_targ, degree):
         xn_avg[i] = temp_avg
 
     weights = np.zeros(degree)
+    # weights = [2, 0, 1]
     bias = 0
 
-    alpha = 1
-
-    num_iters = 5000
     cost_hist = np.empty(num_iters)
-    weight_hist = np.zeros((num_iters, degree + 1))
+    # weight_hist = np.zeros((num_iters, degree + 1))
     # order in arr will be [b, w0, w1...]
-    weight_hist[0][0] = np.float64(bias)
+    # weight_hist[0][0] = np.float64(bias)
     # we are initializing weights at the origin
 
     for i in range(num_iters):
         cost_hist[i] = cost(weights, bias, xn_normed, y_targ)
-        weights, bias = gradient_descent(weights, bias, alpha, xn_normed, y_targ)
+        # weight_hist[i] =
+        weights, bias = gradient_descent(weights, bias, alpha, reg_term, xn_normed, y_targ)
 
     print(cost_hist[num_iters - 1])
-
-    predicted_vals_og = np.zeros(np.shape(x_targ))
-    for j in range(np.shape(x_targ)[0]):
-        predicted_vals_og[j] = eval_polynomial(xn_normed[:, j], weights, bias)
+    print("degree =", degree, "alpha =", alpha, " lambda = ", reg_term)
+    # print(cost_hist[9000], "cost at iter=9000")
+    print(bias, weights)
 
     xs_to_plot = np.linspace(-10, 10, num=100, dtype=np.float64)
     scld_xs = create_scaled_xs(xs_to_plot, degree, xn_rng, xn_avg)
-    print(np.shape(scld_xs))
+    # print(np.shape(scld_xs))
     predicted_vals = np.zeros(np.shape(xs_to_plot))
-    print(np.shape(predicted_vals))
-    print(np.shape(xs_to_plot)[0])
+    # print(np.shape(predicted_vals))
+    # print(np.shape(xs_to_plot)[0])
     for j in range(np.shape(xs_to_plot)[0]):
         # print(j)
         predicted_vals[j] = eval_polynomial(scld_xs[j], weights, bias)
-
 
     # print(weights, bias)
     fig, (ax1, ax2) = plt.subplots(2)
     ax1.plot(np.arange(num_iters), cost_hist)
     ax1.set_title("Cost vs iters")
     ax2.scatter(x_targ, y_targ)
-    ax2.plot(x_targ, predicted_vals_og, 'k')
     ax2.plot(xs_to_plot, predicted_vals, 'r')
     plt.show()
 
@@ -178,8 +175,11 @@ def main():
     # x1_norm, x1_n_rng, x1_n_avg = mean_normed(x_targ, 1)
     # x2_norm, x2_n_rng, x2_n_avg = mean_normed(x_targ, 2)
 
-    degree = 7
-    poly_reg(x_targ, y_targ, degree)
+    degree = 3
+    alpha = .1
+    reg_term = .01
+    num_iters = 10000
+    poly_reg(x_targ, y_targ, degree, alpha, reg_term, num_iters)
 
     # print(x_targ)
     # print(x1_norm)
